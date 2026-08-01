6d2e7f28c2f9d41f1124207c8ac812929458c4a6
Sak1na-IT <pasayevasekine844@gmail.com>
2026-08-01 16:25:39 +0000

    python

---
+++ Node_JS_basic/full_server/utils.js
@@ -0,0 +1,33 @@
+import fs from 'fs';
+
+const readDatabase = (filePath) => new Promise((resolve, reject) => {
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+      return;
+    }
+
+    const lines = data
+      .split('\n')
+      .filter((line) => line.trim() !== '');
+
+    const students = lines.slice(1);
+    const fields = {};
+
+    students.forEach((student) => {
+      const values = student.split(',');
+      const firstName = values[0];
+      const field = values[3];
+
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+
+      fields[field].push(firstName);
+    });
+
+    resolve(fields);
+  });
+});
+
+export default readDatabase;
